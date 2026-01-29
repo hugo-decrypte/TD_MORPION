dca0b83834673a9941270258c4da3981f5c585cd
Hugo Bena <heno.drawing@gmail.com>
2026-01-29 15:54:31 +0100

    Maintenant les 2 joueurs ouvrent un websocket, on a accès aux 2 profils et un autre joueur peut rejoindre la partie, on sait à quel joueur c'est le tour mais faut recharger la page pour l'instant

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -5,68 +5,108 @@ export default {
   data() {
     return {
       partie: null,
-      ownerName: "...waiting",
-      opponentName: "...waiting",
+      ownerName: "...",
+      opponentName: "En attente...",
       code: null,
-      nextPlayer: null
+      nextPlayer: null,
+      socket: null,
+      currentUserId: null
     }
   },
 
   methods: {
-    waitForOpponentMove() {
-      console.log(this.partie)
-      this.socket = new WebSocket('https://morpion-api.edu.netlor.fr/websockets');
-      this.ownerName = this.partie.owner.name
-      this.code = this.partie.code
+
+    //connection sur un websocket ( les 2 joueurus)
+    connectToWebSocket() {
+      if (!this.partie) return;
+
+      this.socket = new WebSocket('wss://morpion-api.edu.netlor.fr/websockets');
 
       this.socket.onopen = () => {
         const message = {
           action: 'connect',
           game_id: this.partie.id,
-          player_id: this.partie.owner_id
+          player_id: this.currentUserId
         };
 
         this.socket.send(JSON.stringify(message));
-        console.log("Connecté au WebSocket, message envoyé !");
+        console.log("Connecté au WebSocket !");
       };
-      api.get(`/api/games/${this.partie.id}`).then(response => {
-        this.partie = response.data
 
-        this.socket.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          console.log(data)
-          switch (data) {
-            case "opponent-join":
-              console.log("Albert vient de rejoindre")
-              this.opponentName = this.partie.opponent.name
-              this.nextPlayer = this.partie.next_player
-              break
-            case "opponent-play":
-              this.nextPlayer = this.partie.next_player
-              console.log("Albert a joué")
-              break
-            case "opponent-quit":
-              console.log("Albert a ff la game")
-              this.opponentName = "...waiting"
-              break
-          }
+      // on reçoit le message du websocket
+      this.socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        console.log("Message WebSocket reçu:", data);
+
+        // On recharge les données de la partie en conséquence
+        this.loadGameData();
+
+        //maintenant faut mettre a jour l'interface en fonction du type de message recu + modifié le jeu morpion en fonction du play
+        switch (data.type || data.action) {
+          case "opponent-join":
+            console.log("L'adversaire a rejoint");
+            break;
+          case "opponent-play":
+            console.log("L'adversaire a joué");
+            break;
+          case "opponent-quit":
+            console.log("L'adversaire a quitté");
+            this.opponentName = "En attente...";
+            break;
         }
-      })
+      };
+
+      this.socket.onerror = (error) => {
+        console.error("Erreur WebSocket:", error);
+      };
+
+      this.socket.onclose = () => {
+        console.log("WebSocket fermé");
+      };
+    },
+
+    loadGameData() {
+      api.get(`/api/games/${this.partie.id}`)
+        .then(response => {
+          this.partie = response.data;
+          this.updateGameInfo();
+        })
+    },
+
+    updateGameInfo() {
+      if (!this.partie) return;
+
+      this.ownerName = this.partie.owner?.name || "Joueur 1";
+      this.opponentName = this.partie.opponent?.name || "En attente...";
+      this.code = this.partie.code;
+      this.nextPlayer = this.partie.next_player;
     }
   },
 
+  // On charge les données de la partie avant d'entrer dans la route
   beforeRouteEnter(to, from, next) {
-    const gameId = to.params['id']
+    const gameId = to.params.id;
+
     api.get(`/api/games/${gameId}`)
       .then(response => {
         next(vm => {
-          vm.partie = response.data
-          vm.waitForOpponentMove()
-        })
+          vm.partie = response.data;
+          vm.updateGameInfo();
+          vm.connectToWebSocket();
+        });
       })
+  },
+
+  // ferme la connexion websocket en quittant la route
+  beforeRouteLeave(to, from, next) {
+    if (this.socket) {
+      this.socket.close();
+    }
+    next();
   }
 }
 </script>
+
 
 <template>
   <h1>Game</h1>
